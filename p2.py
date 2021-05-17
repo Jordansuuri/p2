4526e8ead1b0a0569b3e650f8e129af9b554a2ea
Jordan Chereau <jordansuuri@gmail.com>
2021-05-18 00:30:59 +0200

    commit via Pycharm

--- p2.py
+++ p2.py
@@ -1,4 +1,4 @@
-from bs4 import Beautifulsoup
+from bs4 import BeautifulSoup
 import requests
 
 url = 'http://books.toscrape.com/index.html'
@@ -6,5 +6,5 @@ url = 'http://books.toscrape.com/index.html'
 response = requests.get(url)
 
 if response.ok:
-    soup = Beautifulsoup(response.text)
+    soup = BeautifulSoup(response.text)
     print(soup)
