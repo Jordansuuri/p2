02f11c881678ca07b567dab294e3c40896b77724
Jordan Chereau <jordansuuri@gmail.com>
2021-05-21 00:28:41 +0200

    test pycharm

--- p2.py
+++ p2.py
@@ -1,12 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
+import lxml
 
-url = 'http://books.toscrape.com/index.html'
+# Créer une variable où on rentre l'url qui va nous servir à l'inclure dans la fonction request #
 
+url = 'http://books.toscrape.com/catalogue/its-only-the-himalayas_981/index.html'
+
+# Créer une variable où on utilise l'url avec la methode "get" du module Request. Si on a une bonne reponse (200): on peut demander la suite #
 response = requests.get(url)
 
+#Boucle if : si la reponse est bien 200, on peut demander les informations#
 if response.ok:
-    soup = BeautifulSoup(response.text)
-    print(soup)
-
-print("coucou")
+    content = response.content
+    parser = BeautifulSoup(content, "lxml")
+    body = parser.body
+    print(body.text)
