53b60e5569d2466fb91aa33bc5befc3432dae475
Jordan Chereau <jordansuuri@gmail.com>
2021-05-21 00:47:48 +0200

    recupération du titre avec find.all & la recherche d'une balise

--- p2.py
+++ p2.py
@@ -9,9 +9,16 @@ url = 'http://books.toscrape.com/catalogue/its-only-the-himalayas_981/index.html
 # Créer une variable où on utilise l'url avec la methode "get" du module Request. Si on a une bonne reponse (200): on peut demander la suite #
 response = requests.get(url)
 
-#Boucle if : si la reponse est bien 200, on peut demander les informations#
+# Boucle if : si la reponse est bien 200, on peut demander les informations #
 if response.ok:
+    # recupération du contenu de l'url en brut #
     content = response.content
+    # Variable parser qui utiliser BS #
     parser = BeautifulSoup(content, "lxml")
-    body = parser.body
-    print(body.text)
+    # recuperer le titre du livre et le met dans une liste via find_all#
+    titre_balise = parser.find_all("h1")
+    titre_livre = titre_balise[0].text
+    # Titre recupéré en text
+    print(titre_livre)
+
+
