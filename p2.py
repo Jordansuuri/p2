487f31a0ce7a62580f364763d0d5f370808dc5c2
Jordan Chereau <jordansuuri@gmail.com>
2021-05-21 16:38:49 +0200

    tout les variables dans infos puis ecriture en ligne csv de la liste "info"

--- p2.py
+++ p2.py
@@ -2,16 +2,19 @@ from bs4 import BeautifulSoup
 import requests
 import lxml
 
-# reminder des variables demandées:
+# reminder des variables demandées (dans l'ordre):
+
+
 # product_page_url
-# title
 # universal_product_code
+# title
 # price_including_tax
 # price_excluding_tax
 # number_available
 # product_description
 # category
 # review_rating
+# image_url
 
 # Créer une variable où on rentre l'url qui va nous servir à l'inclure dans la fonction request #
 
@@ -30,50 +33,39 @@ if response.ok:
     td_informations = parser.find_all("td")
 # universal_product_code
     universal_product_code = td_informations[0].text
-# product_description
+# product_description #A CHANGER
     product_description = td_informations[1].text
 # price_excluding_tax
-    price_excluding_tax = td_informations[2].text
+    price_excluding_tax = td_informations[2].text.replace('£','')
 # price_including_tax
-    price_including_tax = td_informations[3].text
+    price_including_tax = td_informations[3].text.replace('£','')
 # number_available
-    number_available = td_informations[5].text
+    number_available = td_informations[5].text.replace('In stock (','').replace(' available)', '')
 # review_rating
-    review_rating = int(td_informations[6].text)
+    review_rating = td_informations[6].text
 # recuperer le titre du livre et le met dans une liste via find_all#
     titre_balise = parser.find_all("h1")
 # title
     title = titre_balise[0].text
 # Category
-#    category_balise = parser.find_all(class_="breadcrumb")
-#    category = category_balise
-#    print(category_balise)
-
-#   print(universal_product_code)
-#   print(title)
-#   print(product_description)
-#   print(price_including_tax)
-#   print(price_excluding_tax)
-#   print(number_available)
-#   print(review_rating)
-
-universal_product_code_list = []
-title_list = []
-product_description_list = []
-price_including_tax_list = []
-price_excluding_tax_list = []
-number_available_list = []
-review_rating_list = []
-category_list = []
-
-# Ajout des informations dans leur listes respectives #
+   # category_balise = parser.find_all(class_="breadcrumb")
+  #  category = category_balise
+ #   print(category_balise)
 
 
-#universal_product_code.append(universal_product_code_list)
-#title.append(title_list)
-#product_description.append(product_description_list)
-#price_including_tax.append(price_including_tax_list)
-#price_excluding_tax.append(price_excluding_tax_list)
-#number_available.append(number_available_list)
-#review_rating.append(review_rating_list)
-#category.append(category_list)
+info = []
+
+# Ajoute les variable la liste info #
+
+info.append(universal_product_code)
+info.append(title)
+info.append(product_description)
+info.append(price_including_tax)
+info.append(price_excluding_tax)
+info.append(number_available)
+info.append(review_rating)
+#info.append(category)
+ligne_csv = ";".join(info)
+
+print(info)
+print(ligne_csv)
