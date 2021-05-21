b65899b6ba584cd809a925b5b9b661837d5f0fff
Jordan Chereau <jordansuuri@gmail.com>
2021-05-21 02:58:24 +0200

    ajout des list, tentative d'append les variables dans des listes respectives. Tentative de trouver category.

--- p2.py
+++ p2.py
@@ -3,6 +3,7 @@ import requests
 import lxml
 
 # reminder des variables demandées:
+# product_page_url
 # title
 # universal_product_code
 # price_including_tax
@@ -31,28 +32,48 @@ if response.ok:
     universal_product_code = td_informations[0].text
 # product_description
     product_description = td_informations[1].text
-# price_including_tax
-    price_including_tax = td_informations[2].text
 # price_excluding_tax
-    price_excluding_tax = td_informations[3].text
+    price_excluding_tax = td_informations[2].text
+# price_including_tax
+    price_including_tax = td_informations[3].text
 # number_available
     number_available = td_informations[5].text
 # review_rating
-    review_rating = td_informations[6].text
+    review_rating = int(td_informations[6].text)
 # recuperer le titre du livre et le met dans une liste via find_all#
     titre_balise = parser.find_all("h1")
 # title
     title = titre_balise[0].text
-# review_rating
+# Category
+#    category_balise = parser.find_all(class_="breadcrumb")
+#    category = category_balise
+#    print(category_balise)
 
-    print(td_informations)
-    print(universal_product_code)
-    print(title)
-    print(product_description)
-    print(price_including_tax)
-    print(price_excluding_tax)
-    print(number_available)
-    print(review_rating)
+#   print(universal_product_code)
+#   print(title)
+#   print(product_description)
+#   print(price_including_tax)
+#   print(price_excluding_tax)
+#   print(number_available)
+#   print(review_rating)
+
+universal_product_code_list = []
+title_list = []
+product_description_list = []
+price_including_tax_list = []
+price_excluding_tax_list = []
+number_available_list = []
+review_rating_list = []
+category_list = []
+
+# Ajout des informations dans leur listes respectives #
 
 
-
+#universal_product_code.append(universal_product_code_list)
+#title.append(title_list)
+#product_description.append(product_description_list)
+#price_including_tax.append(price_including_tax_list)
+#price_excluding_tax.append(price_excluding_tax_list)
+#number_available.append(number_available_list)
+#review_rating.append(review_rating_list)
+#category.append(category_list)
