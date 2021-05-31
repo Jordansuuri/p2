1fe2e358de19496b5118f054b075826e8fdd2a81
Jordan Chereau <jordansuuri@gmail.com>
2021-05-31 04:32:47 +0200

    recupération des liens & des noms de categorie dans une liste respectives

--- extract_category.py
+++ extract_category.py
@@ -19,6 +19,7 @@ for category_text in soup.find_all('a'):
     category_list.append(category_text.get('href'))
 del category_list[0:3]
 del category_list[51:-1]
+del category_list[-2:-1]
 del category_list[-1]
 
 # recuperer les liens et traiter chaque elements de la liste pour ajouter la racine
@@ -26,5 +27,14 @@ for lien_category in category_list:
     link = "http://books.toscrape.com/"+ lien_category
     category_links.append(link)
 
-print(category_links)
 
+#récupération des nom de catégories à partir des liens extraits (convertir en STR, pour chaque lien : split & recuperer la bonne partie
+for category_name in category_list:
+    category_name = str(category_name)
+    category_name_final = category_name.split('catalogue/category/books/')
+    category_name_final2 = category_name_final[1].split('/index.html')
+    category_name_final3 = category_name_final2[0].split('_')
+    category_names.append(category_name_final3[0])
+    print(category_name)
+
+print(category_names)
